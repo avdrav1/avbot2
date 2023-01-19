11a4e407223887cb5ca51685ff242ee35070bf40
avdrav1 <122093670+avdrav1@users.noreply.github.com>
2023-01-19 03:38:40 -0500

    initial commit

---
+++ avbot2.py
@@ -0,0 +1,48 @@
+import os
+import random
+import discord
+
+from dotenv import load_dotenv
+from tinydb import TinyDB, Query
+
+load_dotenv()
+bot = discord.Bot()
+bot_color = 0xFF5733
+
+#Initialize DB
+db = TinyDB('avbot.json')
+regs = ['avdrav', 'aya', 'dol', 'robot', 'toc', 'virtue', 'starsmash', 'ina', 'crash', 'quin', 'soupy']
+
+#Commands
+@bot.event
+async def on_ready():
+    print(f"We have logged in as {bot.user}")
+
+@bot.command(name="regulars", description="Avscord Regulars") 
+async def regulars(ctx):
+    embed=discord.Embed(title="Avscord Regulars", description="Frequent visitors get quotes!", color=bot_color)
+    for r in regs:
+        embed.add_field(name=r, value="", inline=False)
+    await ctx.respond(embed=embed)
+
+@bot.slash_command(name="quote", description="Get a random quote from a regular")
+async def quote(ctx: discord.ApplicationContext, regular: discord.Option(str, autocomplete=discord.utils.basic_autocomplete(regs))):
+    if regular in regs:
+        Quote = Query()
+        results = db.search(Quote.name == regular)
+        q = random.choice(results)
+        embed=discord.Embed(title=q['name'], description=q['quote'], color=bot_color)
+    else:
+        embed=discord.Embed(title="Nope!", description=f"{regular} is not a regular!", color=bot_color)
+    await ctx.respond(embed=embed)
+
+@bot.command(name="ping", description="Sends the bot's latency.") 
+async def ping(ctx): 
+    await ctx.respond(f"Pong! Latency is {bot.latency * 1000}ms")
+
+
+@bot.slash_command(name="lurk", description="Lurk")
+async def lurk(ctx):
+    await ctx.respond("There's no lurking on Avscord!")
+
+bot.run(os.getenv('DISCORD_TOKEN'))
---
+++ avbot2start.sh
@@ -0,0 +1,1 @@
+pm2 start avbot2.py --name "avbot2" --interpreter python3 --watch
