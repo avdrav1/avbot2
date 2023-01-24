02a310d919f19771a56565b3fe76985fff7ce052
avdrav1 <122093670+avdrav1@users.noreply.github.com>
2023-01-24 01:18:24 -0500

    add instagram

--- avbot2.py
+++ avbot2.py
@@ -6,7 +6,7 @@ import asyncpraw
 import pytumblr
 import yfinance
 import tweepy
-import yweather
+import html_to_json
 
 from typing import Literal
 from datetime import date
@@ -18,8 +18,11 @@ from tinydb import TinyDB, Query
 from obliquestrategies import get_strategy
 from discord.ext import tasks, commands
 from discord import option
+from discord import Embed
 from quote import quote
-from newsapi import NewsApiClient   
+from newsapi import NewsApiClient
+from instagrapi import Client   
+from paginator import Paginator, Page
 
 load_dotenv()
 bot = discord.Bot()
@@ -36,9 +39,9 @@ places_map = {}
 woeid_file = open('woeid.json')
 woeid_data = json.load(woeid_file)
 places = woeid_data
-for p in places:
-    key = (p['name']).upper()
-    value = p['woeid']
+for place in places:
+    key = (place['name']).upper()
+    value = place['woeid']
     places_map[key] = value
 #print(places_map['UNITED STATES'])
 
@@ -192,18 +195,18 @@ async def tumblr(
         os.getenv('TUMBLR_TOKEN'),
         os.getenv('TUMBLR_TOKEN_SECRET')
     )
-    
+
     blog_info = tum.blog_info(tumblr_blog)
-    print(json.dumps(blog_info, indent=4))
+    #print(json.dumps(blog_info, indent=4))
     total_posts = blog_info['blog']['total_posts']
     if total_posts > 0:
         await ctx.respond(f"Found blog: {tumblr_blog}")
         posts_json = tum.posts(tumblr_blog, limit=num_stories, offset=random.randint(1,total_posts), type="photo")
-        print(json.dumps(posts_json['posts'], indent=4))
-        for p in posts_json['posts']:
-            print(json.dumps(p['post_url'], indent=4))
-            image_url = p["post_url"].strip('\"')
-            await ctx.send(f'{image_url}')
+        #print(json.dumps(posts_json['posts'], indent=4))
+        for post in posts_json['posts']:
+            print(json.dumps(post, indent=4))
+            image_url = post['post_url']
+            await ctx.send(f"{image_url}")
     else:
         await ctx.respond(f"Could not find blog: {tumblr_blog}")
 
@@ -268,7 +271,7 @@ async def twitter(
     else:
         await ctx.respond(f"Could not find place: {location}")
 
-@bot.slash_command(name="tweets", description="Get the latest headlines")
+@bot.slash_command(name="tweets", description="Get the latest tweets for a given user handle")
 @option("handle", str, description="Enter a twitter handle")
 @option("num_tweets", int, description="Enter the number of tweets to retrieve")
 async def tweets(
@@ -279,22 +282,88 @@ async def tweets(
     auth = tweepy.OAuth2BearerHandler(os.getenv("TWITTER_TOKEN"))
     api = tweepy.API(auth)
 
-    tweets_list = api.user_timeline(screen_name=handle, count=num_tweets)
-    for tweet in tweets_list:
-        #print(json.dumps(tweet._json, indent=4))
-        print(tweet._json["text"])
-        await ctx.send(f"{tweet._json['text']}")
+    paginator = Paginator(bot)
+    pages = []
+
+    try: 
+        tweets_list = api.user_timeline(screen_name=handle, count=num_tweets)
+        await ctx.respond(f"{handle} found")
+        for tweet in tweets_list:
+            #print(json.dumps(tweet._json, indent=4))
+            print(tweet._json["text"])
+            em = Embed(title=handle, url=f"https://twitter.com/{handle}", description=tweet._json["text"])
+            em.set_image(url=tweet._json["user"]["profile_image_url_https"])
+            pages.append(Page(embed=em))        
+            #await ctx.send(f"{tweet._json['text']}")
+        await paginator.send(ctx.channel, pages, type=2, author=ctx.author, disable_on_timeout=False) 
+    except Exception as e:
+        print(e)
+        await ctx.respond(f"{handle} not found")
+
+@bot.slash_command(name="instagram", description="Get the latest pics for a given user handle")
+@option("handle", str, description="Enter an instagram handle")
+@option("num_posts", int, description="Enter the number of posts to retrieve")
+async def instagram(
+    ctx: discord.ApplicationContext,
+    handle: str, 
+    num_posts: int = 3
+):
+    await ctx.respond(f"Loading {num_posts} most recent posts by {handle}...")
+    cl = Client()
+    paginator = Paginator(bot)
+    pages = []
+
+    IG_USERNAME = os.getenv('IG_USERNAME')
+    IG_PASSWORD = os.getenv('IG_PASSWORD')
+    cl.login(IG_USERNAME, IG_PASSWORD)
+    try:
+        user_id = cl.user_id_from_username(handle)
+        print(user_id)
+        medias = cl.user_medias(user_id, num_posts)
+        for m in medias:
+            print(m.dict())
+            if m.thumbnail_url is not None:
+                print(m.thumbnail_url)
+                em = Embed(title=handle, url=f"https://instagram.com/{handle}", description=m.caption_text)
+                em.set_image(url=m.thumbnail_url)
+                pages.append(Page(embed=em))
+                #await ctx.send(m.thumbnail_url)
+                #await ctx.send(m.caption_text)
+            else:
+                for r in m.resources:
+                    print(r.thumbnail_url)
+                    em = Embed(title=handle, url=f"https://instagram.com/{handle}", description=m.caption_text)
+                    em.set_image(url=r.thumbnail_url)
+                    pages.append(Page(embed=em))
+                    #await ctx.send(r.thumbnail_url)
+                #await ctx.send(m.caption_text)
+        await paginator.send(ctx.channel, pages, type=2, author=ctx.author, disable_on_timeout=False)         
+    except Exception as e: 
+        print(e)
+        await ctx.send(f"Can't find any pics by {handle}")
+
+@bot.command(name="pages", description="Testing Embeded Pages") 
+async def pages(ctx):
+    paginator = Paginator(bot)
+
+    pages = [
+        Page(embed=Embed(title="Page #1", description="Testing")),
+        Page(embed=Embed(title="Page #2", description="Still testing")),
+        Page(embed=Embed(title="Page #3", description="Guess... testing"))
+    ]
+
+    await paginator.send(ctx.channel, pages, type=2, author=ctx.author, disable_on_timeout=False)
 
 #Tasks
 @tasks.loop(seconds=60)
 async def send_strategy():
     channel = await bot.fetch_channel(os.getenv('DISCORD_CHANNEL_ID'))
-    print(channel)
+    #print(channel)
     async for message in channel.history(limit=1):
         last_message_timestamp = message.created_at
         sixty_minutes_ago = datetime.now(timezone('UTC')) - timedelta(minutes=60)
-        print(last_message_timestamp)
-        print(sixty_minutes_ago)
+        print(f"Last message in {channel} was at {last_message_timestamp} by {message.author}")
+        #print(sixty_minutes_ago)
         if last_message_timestamp < sixty_minutes_ago:
             await channel.send(f'`{get_strategy()}`')
 
